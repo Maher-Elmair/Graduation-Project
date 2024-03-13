2c61d423c3f6ff979ad9b5d3e2ca2d9c80ada416
Maher-Elmair <maherelmair.jit@email.com>
2024-03-13 08:07:52 +0200

    Just test code online again

--- JavaScript/home_appliances.js
+++ JavaScript/home_appliances.js
@@ -1,9 +1,40 @@
-// add and remove class show-cheek-mark
+// work with show-cheek-mark class name Where it is not repeated more than once
 
-let cardAppliances = document.querySelectorAll(".card")
+let cardHomeAppliances = document.querySelectorAll(".card");
+let cheekBoxMark = document.querySelectorAll(".cheek-box");
 
-for (let i = 0; i < cardAppliances.length; i++) {
-    cardAppliances[i].addEventListener("click", () => {
-        cardAppliances[i].firstElementChild.classList.toggle("show-cheek-mark")
-    })
+
+for (let i = 0; i < cardHomeAppliances.length; i++) {
+    cardHomeAppliances[i].addEventListener("click", function () {
+        for (let n = 0; n < cardHomeAppliances.length; n++) {
+            cheekBoxMark[n].classList.remove("show-cheek-mark");
+        }
+        cheekBoxMark[i].classList.add("show-cheek-mark");
+
+        // work with show-products class name Where it is not repeated more than once
+        let productsCards = document.querySelectorAll(".products-cards ");
+
+        let laundryProducts = document.getElementById("laundry_products")
+        let refrigeratorProducts = document.getElementById("refrigerator_products")
+        let othersProducts = document.getElementById("others_products")
+
+        if (cardHomeAppliances[0].firstElementChild.classList.contains("show-cheek-mark")) {
+            for (let x = 0; x < productsCards.length; x++) {
+                productsCards[x].classList.remove("show-products");
+            }
+            laundryProducts.classList.add("show-products");
+        }
+        if (cardHomeAppliances[1].firstElementChild.classList.contains("show-cheek-mark")) {
+            for (let x = 0; x < productsCards.length; x++) {
+                productsCards[x].classList.remove("show-products");
+            }
+            refrigeratorProducts.classList.add("show-products");
+        }
+        if (cardHomeAppliances[2].firstElementChild.classList.contains("show-cheek-mark")) {
+            for (let x = 0; x < productsCards.length; x++) {
+                productsCards[x].classList.remove("show-products");
+            }
+            othersProducts.classList.add("show-products");
+        }
+    });
 }
